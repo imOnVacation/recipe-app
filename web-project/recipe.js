df5530bb6cb1eedc577ec792ef9ffe3e5b1ec1ee
Vincentmask <60639598+Vincentmask@users.noreply.github.com>
2022-11-24 16:45:05 -0800

    user input chekbox behavior fix

--- web-project/recipe.js
+++ web-project/recipe.js
@@ -1,5 +1,3 @@
-// const apiKey = '73c936ba2f1245898d8cc4a17a7968df';
-
 const clickButtonHandler = (evt) => {
   const result = document.getElementById('receiptSearch').value;
   if (result) {
@@ -24,38 +22,6 @@ checks.on('change', function () {
   results.empty().append(clones);
 });
 
-// $(function () {
-//   $(':checkbox').change(function () {
-//     var arr = $(':checkbox:checked')
-//       .map(function () {
-//         return $(this).next().html();
-//       })
-//       .get();
-//     $('#myDiv').html(arr.join(', '));
-//   });
-// });
-
-// function addcheck() {
-//   var checkbox = document.createElement('input');
-//   checkbox.type = 'checkbox';
-//   checkbox.id = document.getElementById('ingredients').value;
-//   checkbox.name = document.getElementById('ingredients').value;
-//   checkbox.value = document.getElementById('ingredients').value;
-//   checkbox.class = 'campaignCheckBox';
-//   checkbox.checked = true;
-
-//   var label = document.createElement('label');
-//   label.htmlFor = document.getElementById('ingredients').value;
-//   label.appendChild(
-//     document.createTextNode(document.getElementById('ingredients').value)
-//   );
-
-//   var container = document.getElementById('UserInput');
-//   container.appendChild(checkbox);
-//   container.appendChild(label);
-//   event.preventDefault();
-// }
-
 function addcheck() {
   var out = '';
   var value = document.getElementById('ingredients').value;
@@ -63,15 +29,22 @@ function addcheck() {
   if (!value) {
     window.alert('Please enter ingredient');
   } else if (!check) {
-    out += `<label>
-            <input id=${value} name=${value}  type='checkbox' value=${value} checked=true />
-            <for>${value}</for>
+    out += `<label id = ${value}>
+            <input id=${value} name=${value} class="userIn" type='checkbox' value=${value} checked=true onclick="ShowHideUserIn(this.checked, this.value)"/>${value}
           </label>`;
     document.querySelector('#UserInput').innerHTML += out;
   } else {
     window.alert('You already have this ingredient in the box');
   }
   event.preventDefault();
+}
+
+function ShowHideUserIn(ischecked, value) {
+  if (ischecked) $(`#${value}`).show();
+  else {
+    data = document.getElementById(value);
+    data.remove();
+  }
 }
 
 function reset() {
@@ -99,56 +72,3 @@ function GoResult() {
   }
   event.preventDefault();
 }
-
-// function GetRecipe() {
-//   var arr = showSelectedValues().toString().split(',');
-//   var unique = [...new Set(arr)];
-//   unique = encodeURIComponent(unique);
-//   var result = '';
-//   fetch(
-//     `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${unique}&number=30&ranking=2&ignorePantry=true&apiKey=${apiKey}`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       data.forEach((p) => {
-//         var missed = '';
-//         var i = 1;
-//         p.missedIngredients.forEach((miss) => {
-//           missed += `${i}. ${miss.name} <br>`;
-//           ++i;
-//         });
-//         result += `
-//           <div id="keyBoard" class="col-md-3 mt-2" style="display: inline-block">
-//               <div class="card" style="width: 18rem;">
-//                   <img src="${p.image}" class="card-img-top img-fluid" alt="keyboard">
-//                   <div class="card-body">
-//                       <h5 class="card-title" id="itemName">${p.title}</h5>
-//                       <p class="card-text" id="itemDesc">Missed Ingredients Count: ${p.missedIngredientCount}</p>
-//                       <p class="card-text""><h6>Missed Ingredients:</h6> ${missed}</p>
-//                       <p class="card-text">Used ingredients Count: ${p.usedIngredientCount}</p>
-//                       <a href="#" class="btn btn-primary" id="redirect" onClick = "GetURL(${p.id})">Get instruction</a>
-//                   </div>
-//               </div>
-//           </div>`;
-//       });
-//       document.querySelector('#Out').innerHTML = result;
-//     })
-//     .catch(() => {
-//       console.log('error');
-//     });
-//   event.preventDefault();
-// }
-
-// function GetURL(id) {
-//   fetch(
-//     `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&apiKey=${apiKey}`
-//   )
-//     .then((response) => response.json())
-//     .then((data) => {
-//       location.href = `${data.spoonacularSourceUrl}`;
-//     })
-//     .catch(() => {
-//       console.log('error');
-//     });
-//   event.preventDefault();
-// }
